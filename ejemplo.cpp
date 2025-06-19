44bda09799200ce57253962288e4f8cde355d244
Lucia-An119 <languloy@unjbg.edu.pe>
2025-06-18 20:03:27 -0500

    se agregó enunciados

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -10,13 +10,19 @@ struct EMP{
 int main(){
     EMP EMPLEADOS[100];
     int n;
+    cout<<"numeros de empleados: ";
     cin>>n;
     for(int i=0; i<n; i++){
+    	cout<<"Numero de empleado : ";
         cin>>EMPLEADOS[i].num;
         cin.ignore();
+        cout<<"Nombre del empleado: ";
         getline(cin,EMPLEADOS[i].nom);
-        for(int j=0; j<12; j++)
+        cout<<"Ventas por mes: ";
+        for(int j=0; j<12; j++){
             cin>>EMPLEADOS[i].ven[j];
+        }
+        cout<<"Salario: ";
         cin>>EMPLEADOS[i].sal;
     }
     return 0;
